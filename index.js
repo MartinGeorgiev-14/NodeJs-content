9d118645d182a86e648bbed7a3978d6216a7e0be
MartinGeorgiev-14 <georgiev632@gmail.com>
2024-05-22 00:34:13 +0300

    integrated mongodb database

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require("http")
 const { json } = require("stream/consumers")
 
+require('dotenv').config()
 const cors = require("cors")
 const express = require("express")
+const app = express()
 const { error } = require("console")
+const mongoose = require("mongoose")
+const Note = require("./note")
 
 const requestLogger = (request, response, next) => {
     console.log("Method:", request.method)
@@ -13,11 +17,20 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
-const app = express()
+
+
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
 app.use(express.static("dist"))
+
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
+
 
 let notes = [
     {
@@ -43,48 +56,39 @@ app.get("/", (request, response) => {
 })
 
 app.get("/api/notes", (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-
-    const note = notes.find(note => note.id === id)
-
-    if(note){
-        response.json(note) 
-    }
-    else{
-        response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+       })
 })
 
 app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    return response.status(204).end()
+    Note.findById(request.params.id).then(note => {
+    response.json(note)
+   })
 })
 
 app.post("/api/notes", (request, response) => {
-  
-    const body = request.body
+  const body = request.body
 
-    if(!body.content){
-        return response.status(400).json({
-            error: "content missing"
-        })
-    }
+  if(body.content === undefined ){
+    return response.status(400).json({error: "content missing"})
+  }
+    
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId()
-    }
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 
-    notes = notes.concat(note)
-
-    return response.json(note)
 })
 
 const unknownEndpoint = (request, response) => {
@@ -101,7 +105,3 @@ const generateId = () => {
     return maxId + 1
 }
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
