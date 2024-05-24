81bed7358f4a22ac671a1d4b151efa115de35f91
MartinGeorgiev-14 <georgiev632@gmail.com>
2024-05-25 01:47:20 +0300

    updated backend

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const requestLogger = (request, response, next) => {
 }
 
 
-
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
@@ -61,22 +60,47 @@ app.get("/api/notes", (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
-       })
+        if(note){
+            response.json(note)
+        }
+        else{
+            response.status(404).end()
+        }
+       }).catch(error => next(error))   
 })
 
-app.delete("/api/notes/:id", (request, response) => {
-    Note.findById(request.params.id).then(note => {
-    response.json(note)
-   })
+app.delete("/api/notes/:id", (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id).then(note => {
+        response.status(204).end()
+    }).catch(error => next(error))
 })
 
-app.post("/api/notes", (request, response) => {
+app.put("/api/notes/:id", (request, response, next) => {
+    const {content, important} = request.body
+
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important || false
+    }
+
+    Note.findByIdAndUpdate(request.params.id, 
+        {content, important},
+        {new: true, runValidators: true, context: "query"} )
+    .then(updatedNote => {
+        response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.post("/api/notes", (request, response, next) => {
   const body = request.body
 
-  if(body.content === undefined ){
+  console.log(body.content)
+  if(body.content === undefined){
     return response.status(400).json({error: "content missing"})
   }
     
@@ -88,14 +112,27 @@ app.post("/api/notes", (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 
 })
+
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({error: "unknown endpoint"})
 }
 
 app.use(unknownEndpoint)
+
+const errorHander = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name === "CastError"){
+        return response.status(400).send({error: "malformatted id"})
+    }else if(error.name === "ValidationError"){
+        return response.status(400).json({error: error.message})
+    }
+}
+app.use(errorHander)
 
 const generateId = () => {
     const maxId = notes.length > 0
